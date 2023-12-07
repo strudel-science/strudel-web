5d044ed2f961614698927e6e3a02dcee8c299909
Cody O'Donnell <ctodonnell@lbl.gov>
2023-12-06 16:54:14 -0800

    Add path prefix for github pages build

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
+  pathPrefix: '/strudel-web',
   siteMetadata: {
     title: `STRUDEL`,
     siteUrl: `https://strudel.science`,
