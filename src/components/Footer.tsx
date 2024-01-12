2c8890089f7f2962b8e9197c6eeb7eb4fa584ef3
Cody O'Donnell <ctodonnell@lbl.gov>
2024-01-12 15:28:58 -0800

    Add icons to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
-import { Box, Breakpoint, Container } from '@mui/material';
+import { Box, Breakpoint, Container, Stack, Typography } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import MarkAsUnreadIcon from '@mui/icons-material/MarkAsUnread';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import { Link } from '@mui/material';
 
 interface FooterProps {
@@ -30,19 +32,32 @@ export const Footer: React.FC<FooterProps> = ({
             color: 'purple'
           }
         }}
-      >
-       
-        <strong>Questions? Ideas? Looking to stay up to date?</strong>
-        
-        <ul>
-          <li>Get in touch at <Link href="mailto:strudel@lbl.gov" target="_blank">strudel@lbl.gov</Link></li>
-          <li>Join our mailing list by emailing <Link href="mailto:strudel-community+subscribe@lbl.gov" target="_blank">strudel-community+subscribe@lbl.gov</Link></li>
-          <li>Visit us on <Link href='https://github.com/strudel-science/' target="_blank">GitHub</Link> and join the <Link href='https://github.com/orgs/strudel-science/discussions/' target='_blank'>Discussion</Link></li>
-        </ul>
+      >        
+        <Stack spacing={1}>
+          <Typography component="h6" fontWeight="bold">Questions? Ideas? Looking to stay up to date?</Typography>
+          <Stack direction="row" spacing={1}>
+            <SendIcon />
+            <Typography>
+              Get in touch at <Link href="mailto:strudel@lbl.gov" target="_blank">strudel@lbl.gov</Link>
+            </Typography>
+          </Stack>
+          <Stack direction="row" spacing={1}>
+            <MarkAsUnreadIcon />
+            <Typography>
+              Join our mailing list by emailing <Link href="mailto:strudel-community+subscribe@lbl.gov" target="_blank">strudel-community+subscribe@lbl.gov</Link>
+            </Typography>
+          </Stack>
+          <Stack direction="row" spacing={1}>
+            <GitHubIcon />
+            <Typography>
+              Visit us on <Link href='https://github.com/strudel-science/' target="_blank">GitHub</Link> and join the <Link href='https://github.com/orgs/strudel-science/discussions/' target='_blank'>Discussion</Link>
+            </Typography>
+          </Stack>
+        </Stack>
         <br />
         <em>STRUDEL is an effort of the Berkeley Lab Scientific Data <Link href="https://crd.lbl.gov/divisions/scidata/" target="_blank" rel="noreferrer">(SciData)</Link> Division <Link href="https://ux.lbl.gov" target="_blank" rel="noreferrer">UX team</Link></em>.
         <br />
-        <em>STRUDEL is generously funded by the Alfred P. Sloan Foundation, Liz Vu & Josh Greenberg Program Officers, initial grant <Link href="https://sloan.org/grant-detail/10074" target="_blank" rel="noreferrer">#10074</Link></em>
+        <em>The project is generously funded by the Alfred P. Sloan Foundation, Liz Vu & Josh Greenberg Program Officers, initial grant <Link href="https://sloan.org/grant-detail/10074" target="_blank" rel="noreferrer">#10074</Link></em>
         
       </Container>
     </Box>
